80981696c1dde1220abc4dcb28291229e6a76533
Kirill Kanti <kirillzhitko@gmail.com>
2022-04-13 16:58:59 +0300

    feat: add IDataBase ::count, ::clear()
    feat: add IEventTodoDB ::onClear

--- app/src/main/java/kartex/tododer/lib/model/IEventTodoDB.kt
+++ app/src/main/java/kartex/tododer/lib/model/IEventTodoDB.kt
@@ -2,10 +2,12 @@ package kartex.tododer.lib.model
 
 import kartex.tododer.lib.todo.ITodo
 import savvy.toolkit.Event
+import savvy.toolkit.EventArgs
 
 interface IEventTodoDB<Todo : ITodo> : ITodoDB<Todo> {
 
 	val onAdd: Event<TodoDBEventArgs<Todo>>
 	val onEdit: Event<TodoDBEventArgs<Todo>>
 	val onRemove: Event<TodoDBEventArgs<Todo>>
+	val onClear: Event<EventArgs>
 }
