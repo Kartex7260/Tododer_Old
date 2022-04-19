6f96e483bc98efcd5e66d5065db275926d3ea6e3
Kirill Kanti <kirillzhitko@gmail.com>
2022-04-19 11:59:01 +0300

    fix: sort by title

--- app/src/main/java/kartex/tododer/ui/sort/SortByTitle.kt
+++ app/src/main/java/kartex/tododer/ui/sort/SortByTitle.kt
@@ -5,7 +5,7 @@ import kartex.tododer.lib.todo.ITodo
 
 class SortByTitle private constructor(reverse: Boolean = false) : TodoSort(0, R.drawable.sort_alphabet_24, reverse) {
 
-	override fun <T : ITodo> sort0(iterable: Iterable<T>) = iterable.sortedBy { it.title }
+	override fun <T : ITodo> sort0(iterable: Iterable<T>) = iterable.sortedBy { it.title.lowercase() }
 
 	override fun asReverse() = if (reverse)
 		INST
